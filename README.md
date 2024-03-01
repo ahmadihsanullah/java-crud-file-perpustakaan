b43760c31f86ee7b581759511f37988bb25464da
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-03-01 20:17:40 +0700

    java crud menggunakan file sebagai database

---
+++ README.md
@@ -0,0 +1,7 @@
+## Java - CRUD
+Membuat project CRUD sederhana, penyimpanan berupa file.txt sebagai databasenya. 
+- Memanfaatkan operasi FILE
+
+ngikutin tutorial dari` kelas terbuka:
+
+https://youtu.be/XIhlH-t5HL0?si=Bxz-gsYIfPCZ-gyn
---
+++ src/App.java
@@ -0,0 +1,69 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+import operations.*;
+import utility.*;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        try (Scanner terminalInput = new Scanner(System.in)) {
+            String pilihanUser;
+            boolean isLanjutkan = true;
+
+            while (isLanjutkan) {
+                Util.clearScreen();
+                System.out.println("Database Perpustakaan\n");
+                System.out.println("1.\tLihat seluruh buku");
+                System.out.println("2.\tCari data buku");
+                System.out.println("3.\tTambah data buku");
+                System.out.println("4.\tUbah data buku");
+                System.out.println("5.\tHapus data buku");
+
+                System.out.print("\n\nPilihan anda: ");
+                pilihanUser = terminalInput.next();
+
+                switch (pilihanUser) {
+                    case "1":
+                        System.out.println("\n=================");
+                        System.out.println("LIST SELURUH BUKU");
+                        System.out.println("=================");
+                        Operation.tampilkanData();
+                        break;
+                    case "2":
+                        System.out.println("\n=========");
+                        System.out.println("CARI BUKU");
+                        System.out.println("=========");
+                        Operation.cariData();
+                        // cari data
+                        break;
+                    case "3":
+                        System.out.println("\n================");
+                        System.out.println("TAMBAH DATA BUKU");
+                        System.out.println("================");
+                        // tambah data
+                        Operation.tambahData();
+                        break;
+                    case "4":
+                        System.out.println("\n==============");
+                        System.out.println("UBAH DATA BUKU");
+                        System.out.println("==============");
+                        // ubah data
+                        Operation.updateData();
+                        break;
+                    case "5":
+                        System.out.println("\n===============");
+                        System.out.println("HAPUS DATA BUKU");
+                        System.out.println("===============");
+                        // hapus data
+                        Operation.hapusData();
+                        break;
+                    default:
+                        System.err.println("\nInput anda tidak ditemukan\nSilahkan pilih [1-5]");
+                }
+
+                isLanjutkan = Util.getYesorNo("Apakah Anda ingin melanjutkan");
+            }
+        }
+    }
+}
